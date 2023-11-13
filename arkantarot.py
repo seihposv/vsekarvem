bad7463bff5dc29fd9e002c45a2315e7392f2a6b
Sophie <146029588+seihposv@users.noreply.github.com>
2023-11-13 16:58:56 +0300

    Add files via upload

---
+++ arkantarot.py
@@ -0,0 +1,36 @@
+number = int(input("Введите число от 0 до 21: "))
+if number < 0 or number > 21:
+    print("Число должно быть от 0 до 21")
+elif number == 0:
+    print("Вы выбрали аркан 'Дурак'")
+elif number == 1:
+    print("Вы выбрали аркан 'Маг'")
+elif number == 2:
+    print("Вы выбрали аркан 'Верховная Жрица'")
+elif number == 3:
+    print("Вы выбрали аркан 'Императрица'")
+elif number == 4:
+    print("Вы выбрали аркан 'Император'")
+elif number == 5:
+    print("Вы выбрали аркан 'Иерофант'")
+elif number == 6:
+    print("Вы выбрали аркан 'Влюбленные'")
+elif number == 7:
+    print("Вы выбрали аркан 'Колесница'")
+elif number == 8:
+    print("Вы выбрали аркан 'Сила'")
+elif number == 9:
+    print("Вы выбрали аркан 'Отшельник'")
+elif number == 10:
+    print("Вы выбрали аркан 'Колесо Фортуны'")
+elif number == 11:
+    print("Вы выбрали аркан 'Справедливость'")
+elif number == 12:
+    print("Вы выбрали аркан 'Повешенный'")
+elif number == 13:
+    print("Вы выбрали аркан 'Смерть'")
+elif number == 14:
+    print("Вы выбрали аркан 'Умеренность'")
+elif number == 15:
+    print("Вы выбрали аркан 'Дьявол'")
+
